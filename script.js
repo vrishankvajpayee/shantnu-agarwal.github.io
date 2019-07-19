70a1881025ded008c6241e4ac45e8e2631694be8
Shantnu Agarwal <shantnu.agarwal@gmail.com>
2019-07-19 16:20:34 +0530

    alter UI, add smooth scroll

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myBtn").style.display = "block";
+    document.getElementById("scrollBtn").style.display = "block";
   } else {
-    document.getElementById("myBtn").style.display = "none";
+    document.getElementById("scrollBtn").style.display = "none";
   }
 }
 
